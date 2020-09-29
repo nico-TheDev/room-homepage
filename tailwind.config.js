85d617e1a8963938bb3ebb1c34792ef69d944dfd
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-09-29 13:51:44 +0800

    add utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
         removeDeprecatedGapUtilities: true,
         purgeLayersByDefault: true,
     },
-    purge: [],
+    purge: ["index.html"],
     theme: {
         extend: {
             colors: {
                 DarkGray: "hsl(0, 0%, 63%)",
                 VeryDarkGray: "hsl(0, 0%, 27%)",
+                overlay: "rgba(0,0,0,0.7)",
             },
             fontSize: {
                 default: "12px",
@@ -18,6 +19,20 @@ module.exports = {
             },
             width: {
                 90: "90%",
+            },
+            height: {
+                "10vh": "10vh",
+                "70vh": "70vh",
+                "30vh": "30vh",
+            },
+            gridTemplateRows: {
+                split: "50vh auto",
+                desktop: "65vh 35vh",
+            },
+            letterSpacing: {
+                1: "5px",
+                2: "7px",
+                3: "9px",
             },
         },
     },
